d9726b5a2ae0717d5fd4c114fae48df695791142
mohamedabdi4h <mohamedabdikaniahmed@gmail.com>
2025-07-06 20:08:08 +0300

    Done

---
+++ nextjs/day2-database/prisma/migrations/20250706152956_post/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT,
+    "published" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" INTEGER NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
